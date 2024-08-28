2776cfe86a7045bc40abb544e70aba95d6c0b23c
iñaki llorens <inakillorens@Laptop-de-inaki.local>
2024-08-28 11:35:47 -0300

    Fix typo in Direction

--- src/direction.rs
+++ src/direction.rs
@@ -1,4 +1,4 @@
-/// The Direction enum represents wheather a hash must be concatenated to the left or to the right.
+/// The Direction enum represents whether a hash must be concatenated to the left or to the right.
 #[derive(Debug)]
 pub enum Direction {
     Left,
